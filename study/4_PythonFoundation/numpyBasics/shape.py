a5762aeccee52d158f36dd1c3dd3c59c724b7cd5
AlbertCAO <364296356@qq.com>
2017-10-17 15:04:55 +0800

    update shape.py

--- study/4_PythonFoundation/numpyBasics/shape.py
+++ study/4_PythonFoundation/numpyBasics/shape.py
@@ -4,7 +4,8 @@ Created on Oct 17, 2017
 @author:
     CaoZhen
 @description:
-    numpy.ndarray.shape (difference between np.array & np.mat)
+    1. numpy.ndarray.shape (difference between np.array & np.mat)
+    2. review the usage of flatten()/ravel()/newaxis
 @reference:
     1. numpy.ndarray.shape in Dash
 '''
@@ -12,13 +13,28 @@ Created on Oct 17, 2017
 import numpy as np
 
 a1Arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-a1Mat = np.mat(a1Arr)
-
 print a1Arr
 print np.shape(a1Arr)
 
-print a1Mat
+a1Arr_2 = a1Arr[:, np.newaxis]
+print a1Arr_2
+print np.shape(a1Arr_2)
+
+a1Arr_3 = a1Arr_2.flatten()
+print a1Arr_3
+print np.shape(a1Arr_3)
+
+a1Arr_4 = a1Arr_2.ravel()
+print a1Arr_4
+print np.shape(a1Arr_4)
+
+a1Mat = np.mat(a1Arr)
+print 'a1Mat : ', a1Mat
 print np.shape(a1Mat)
+
+a1Mat_2 = a1Mat.T
+print a1Mat_2
+print np.shape(a1Mat_2)
 
 a2Arr = np.array([[0, 1, 2, 3, 4],[5, 6 ,7, 8, 9]])
 a2Mat = np.mat(a2Arr)
