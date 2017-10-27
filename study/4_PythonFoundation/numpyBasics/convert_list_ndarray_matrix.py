95998091e433ee4a582700376bc6109a82a94b84
AlbertCAO <364296356@qq.com>
2017-10-27 17:49:29 +0800

    add convert between list / ndarray / matrix

---
+++ study/4_PythonFoundation/numpyBasics/convert_list_ndarray_matrix.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+'''
+Created on Oct 27, 2017
+@author:
+    CaoZhen
+@description:
+    1. Convert between list / ndarray / matrix
+@reference:
+    1. 
+'''
+
+import numpy as np
+
+aList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# 1. list to ndarray / matrix
+aArr = np.array(aList)
+aMat = np.mat(aList)
+print aArr
+print aMat
+
+# 2. matrix to ndarray
+try:
+    print aMat.toarray()
+except Exception, e:
+    print e
+print np.asarray(aMat)
+print np.array(aMat)
+
+# 3. ndarray - row to col
+aArr2 = aArr[:, np.newaxis]
+aMat2 = np.mat(aArr2)
+print aArr2, type(aArr2)
+print aMat[:, np.newaxis], type(aMat[:, np.newaxis])
+
+# 4. ndarray - col to row
+# Convert ndarray with 1 column to an array with M elements
+print np.squeeze(aArr2)
+print aArr2.reshape(-1)
+print aArr2.flatten()
+print aArr2.ravel()
+# directly
+print aMat2.A1
