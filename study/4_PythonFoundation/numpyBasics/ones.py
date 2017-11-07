87d1c320a8ccbcff0cd1c6eaebd79a76a53c5123
AlbertCAO <364296356@qq.com>
2017-11-07 15:03:08 +0800

    add ones.py

---
+++ study/4_PythonFoundation/numpyBasics/ones.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+'''
+Created on Nov 07, 2017
+@author:
+    CaoZhen
+@description:
+    1. generate ones array or matrix
+       comment: attention the input format difference
+@reference:
+    1. 
+'''
+
+import numpy as np
+
+a1 = np.ones(10)
+print a1, type(a1)
+
+a2 = np.ones((10, 1))
+print a2, type(a2)
