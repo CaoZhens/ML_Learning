66217309d41e21bec2728657a06b7a68534a42b4
AlbertCAO <364296356@qq.com>
2017-11-14 18:11:38 +0800

    update multiply.py

--- study/4_PythonFoundation/numpyBasics/multiply.py
+++ study/4_PythonFoundation/numpyBasics/multiply.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 '''
 Created on Nov 07, 2017
-Updated on Nov 08, 2017
+Updated on Nov 08, 2017 / Nov 14, 2017
 @author:
     CaoZhen
 @description:
@@ -20,18 +20,21 @@ import numpy as np
 # Desc:
 # a. for 1-D arrays, it is equivalent to inner product of vectors
 # comment: inner product equivalent to dot product
+print '\n==========1. scalars=========='
 a = 2.0
 b = 5.0
 print 'a =', a, 'b =', b
 print 'a * b =', a * b
 print 'np.dot(a, b) =', np.dot(a, b)
+print 'np.multiply(a, b) =', np.multiply(a, b)
 
-
+print '\n==========2. 1-D array=========='
 a = np.arange(1, 5)
 b = np.arange(2, 6)
 print 'a =', a, 'b =', b
 print 'a * b =', a * b
 print 'np.dot(a, b) =', np.dot(a, b)
+print 'np.multiply(a, b) =', np.multiply(a, b)
 try:
     print 'np.mat(a) * np.mat(b) =', np.mat(a) * np.mat(b)
 except Exception, e:
@@ -40,42 +43,20 @@ try:
     print 'np.dot(np.mat(a), np.mat(b)) =', np.dot(np.mat(a), np.mat(b))
 except Exception, e:
     print e
+print 'np.multiply(np.mat(a), np.mat(b)) =', np.multiply(np.mat(a), np.mat(b))
 
 # b. For 2-D arrays it is equivalent to matrix multiplication
+print '\n==========3. 2-D array=========='
 a = np.arange(1, 5).reshape(2, -1)
 b = np.arange(2, 6).reshape(2, -1)
 print 'a =', a
 print 'b =', b
 print 'a * b =', a * b
 print 'np.dot(a, b) =', np.dot(a, b)
-
 print 'np.mat(a) * np.mat(b) =', np.mat(a) * np.mat(b)
 print 'np.dot(np.mat(a), np.mat(b)) =', np.dot(np.mat(a), np.mat(b))
+print 'np.multiply(np.mat(a), np.mat(b)) =', np.multiply(np.mat(a), np.mat(b))
 # 2. np.multiply
 # Desc    : <ufunc multiply> Multiply arguments element-wise.
 # Notes   : Equivalent to x1 * x2 in terms of array broadcasting.
 # Comment : ufunc - universal function
-
-a = np.arange(1, 5)
-b = np.arange(2, 6)
-print 'a =', a
-print 'b =', b
-print 'a * b =', a * b
-print 'np.dot(a, b) =', np.dot(a, b)
-print 'np.multiply(a, b) =', np.multiply(a, b)
-
-try:
-    print 'np.mat(a) * np.mat(b) =', np.mat(a) * np.mat(b)
-except Exception, e:
-    print e
-try:
-    print 'np.dot(np.mat(a), np.mat(b)) =', np.dot(np.mat(a), np.mat(b))
-except Exception, e:
-    print e
-
-print 'np.multiply(np.mat(a), np.mat(b)) =', np.multiply(np.mat(a), np.mat(b))
-# x1 = np.arange(9.0).reshape((3, 3))
-# x2 = np.arange(3.0)
-# print x1
-# print x2
-# # np.multiply(x1, x2)
