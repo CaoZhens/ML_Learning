e9eb6da2db7a84dac571007206ecf931faf87ae8
AlbertCAO <364296356@qq.com>
2017-11-29 13:16:19 +0800

    update multiply.py

--- study/4_PythonFoundation/numpyBasics/multiply.py
+++ study/4_PythonFoundation/numpyBasics/multiply.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 '''
 Created on Nov 07, 2017
-Updated on Nov 08, 2017 / Nov 14, 2017
+Updated on Nov 08, 2017 / Nov 14, 2017 / Nov 29, 2017
 @author:
     CaoZhen
 @description:
     1. multiply operation between ndarray & matrix
-    2. np.dot / np.multiply / *
+    2. np.dot / np.multiply / * / **
 @reference:
     1. numpy.multiply in Dash
     2. numpy.dot      in Dash
@@ -23,8 +23,10 @@ import numpy as np
 print '\n==========1. scalars=========='
 a = 2.0
 b = 5.0
-print 'a =', a, 'b =', b
+print 'a =', a, ', b =', b
 print 'a * b =', a * b
+print 'a ** 2 =', a ** 2
+print 'a ** b =', a ** b
 print 'np.dot(a, b) =', np.dot(a, b)
 print 'np.multiply(a, b) =', np.multiply(a, b)
 
@@ -33,10 +35,16 @@ a = np.arange(1, 5)
 b = np.arange(2, 6)
 print 'a =', a, 'b =', b
 print 'a * b =', a * b
+print 'a ** 2 =', a ** 2
+print 'a ** b =', a ** b 
 print 'np.dot(a, b) =', np.dot(a, b)
 print 'np.multiply(a, b) =', np.multiply(a, b)
 try:
     print 'np.mat(a) * np.mat(b) =', np.mat(a) * np.mat(b)
+except Exception, e:
+    print e
+try:
+    print 'np.mat(a) ** 2 =', np.mat(a) ** 2
 except Exception, e:
     print e
 try:
@@ -49,11 +57,18 @@ print 'np.multiply(np.mat(a), np.mat(b)) =', np.multiply(np.mat(a), np.mat(b))
 print '\n==========3. 2-D array=========='
 a = np.arange(1, 5).reshape(2, -1)
 b = np.arange(2, 6).reshape(2, -1)
-print 'a =', a
-print 'b =', b
-print 'a * b =', a * b
+print 'a =\n', a
+print 'b =\n', b
+print 'a * b =\n', a * b
+print 'a ** 2 =\n', a ** 2
+print 'a ** b =\n', a ** b 
 print 'np.dot(a, b) =', np.dot(a, b)
 print 'np.mat(a) * np.mat(b) =', np.mat(a) * np.mat(b)
+print 'np.mat(a) ** 2 =', np.mat(a) ** 2
+try:
+    print 'np.mat(a) ** np.mat(b) =', np.mat(a) ** np.mat(b)
+except Exception, e:
+    print e
 print 'np.dot(np.mat(a), np.mat(b)) =', np.dot(np.mat(a), np.mat(b))
 print 'np.multiply(np.mat(a), np.mat(b)) =', np.multiply(np.mat(a), np.mat(b))
 # 2. np.multiply
